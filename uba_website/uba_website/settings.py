fab31011f0ffc0737323e576fa95de4b004797dc
Michael Kang <yk2854@nyu.edu>
2023-09-06 15:54:21 +0800

    build.sh updates

--- uba_website/uba_website/settings.py
+++ uba_website/uba_website/settings.py
@@ -132,6 +132,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATTIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
